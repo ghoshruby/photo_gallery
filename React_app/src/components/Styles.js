03e46edb06befc972260c71a48aacb84cbc15e3a
Ruby Ghosh <60173326+ghoshruby@users.noreply.github.com>
2021-06-17 15:16:13 +0530

    Add files via upload

---
+++ React_app/src/components/Styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+	
+export const Header = styled.h1`
+text-align: center;
+left: 50%;
+color: green;
+`;
+	
+export const Content = styled.div`
+overflowY: scroll;
+height: 100px;
+`;
+	
+export const Button = styled.div`
+position: fixed;
+width: 100%;
+top:80%;
+
+align:center;
+height: 20px;
+font-size: 3rem;
+z-index: 1;
+cursor: pointer;
+color: #1C5DDB;
+`;
+
+export const Button1 = styled.div`
+position: fixed;
+width: 100%;
+top:85%;
+left: 47%;
+align:center;
+height: 20px;
+font-size: 3rem;
+z-index: 1;
+cursor: pointer;
+color: #E1090C;
+`
